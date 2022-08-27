9e51d83cd22ac92658aac9661d41b1ea80c9b09e
alfonsusrr <alfonsus737@gmail.com>
2022-08-27 11:51:15 +0700

    Fix error build

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   swcMinify: true,
   env: {
     FIREBASE_AUTH: process.env.FIREBASE_AUTH,
-    MAPS_API_KEY: process.env.MAPS_API_KEY
+    MAPS_API_KEY: process.env.MAPS_API_KEY,
+    SERVICE_ACCOUNT_KEY: process.env.SERVICE_ACCOUNT_KEY
   },
   images: {
     domains: ['img.icons8.com']
